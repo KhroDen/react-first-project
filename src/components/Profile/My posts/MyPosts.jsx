fd9fdca4f1c22316d2d87b3092921ff8f17bd44f
KhroDen <dg.hromov@gmail.com>
2021-12-13 21:10:37 +0200

    подняли входящие данные на уровень вверх MyPosts -> Profile

--- src/components/Profile/My posts/MyPosts.jsx
+++ src/components/Profile/My posts/MyPosts.jsx
@@ -3,19 +3,22 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 
-let posts = [
+/*let posts = [
   { id: 1, post: 'Hi, how are you?', likesCount: 14 },
   { id: 2, post: "It's my first post", likesCount: 21 }
-]
-
-let postsElements = posts.map(p => {
-  return (
-    <Post message={p.post} likesCount={p.likesCount} />
-  )
-})
+]*/
 
 
-const MyPosts = () => {
+
+
+const MyPosts = (props) => {
+
+  let postsElements = props.posts.map(p => {
+    return (
+      <Post message={p.post} likesCount={p.likesCount} />
+    )
+  })
+
   return (
     <div className={s.postBlock}>
       <h3>My posts</h3>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import MyPosts from "./My posts/MyPosts";
-import s from './Profile.module.css';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
+
+  let posts = [
+    { id: 1, post: 'Hi, how are you?', likesCount: 14 },
+    { id: 2, post: "It's my first post", likesCount: 21 }
+  ]
+
   return <div>
     <ProfileInfo />
-    <MyPosts />
+    <MyPosts posts={posts} />
   </div>
 }
 
