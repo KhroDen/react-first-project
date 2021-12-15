ade3ba667c21b2b62dd0ce85ef3c3af0119e4e3a
KhroDen <dg.hromov@gmail.com>
2021-12-15 17:18:57 +0200

    удалил данные из index

--- src/index.js
+++ src/index.js
@@ -5,27 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from './redux/state';
 
-/*let posts = [
-  { id: 1, post: 'Hi, how are you?', likesCount: 14 },
-  { id: 2, post: "It's my first post", likesCount: 21 }
-]
-
-let dialogs = [
-  { id: 1, name: 'Dimych' },
-  { id: 2, name: 'Andrey' },
-  { id: 3, name: 'Sveta' },
-  { id: 4, name: 'Sasha' },
-  { id: 5, name: 'Viktor' },
-  { id: 6, name: 'Valera' },
-]
-
-let messages = [
-  { id: 1, message: 'Hi' },
-  { id: 2, message: 'How is your it-kama?' },
-  { id: 3, message: 'Yo' },
-  { id: 4, message: 'Yo' },
-  { id: 5, message: 'Yo' }
-]*/
 
 ReactDOM.render(
   <React.StrictMode>
