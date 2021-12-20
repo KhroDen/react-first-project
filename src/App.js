8d7c3a1065c7e8dac3f1ced7eefb7e9a21132edd
KhroDen <dg.hromov@gmail.com>
2021-12-20 19:22:07 +0200

    vse tak je

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
 import { Route, Routes } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
