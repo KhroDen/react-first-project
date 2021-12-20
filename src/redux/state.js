4e18c77cfa7db40c7690323d900e15c490222d39
KhroDen <dg.hromov@gmail.com>
2021-12-20 17:16:12 +0200

    add function in state, which will be add new post in profilePage.posts

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,29 +1,40 @@
 let state = {
 
-profilePage: {
-    posts: [
-        { id: 1, post: 'Hi, how are you?', likesCount: 14 },
-        { id: 2, post: "It's my first post", likesCount: 21 }
-    ]},
+    profilePage: {
+        posts: [
+            { id: 1, post: 'Hi, how are you?', likesCount: 14 },
+            { id: 2, post: "It's my first post", likesCount: 21 }
+        ]
+    },
 
-dialogsPage: {
-    dialogs: [
-        { id: 1, name: 'Dimych' },
-        { id: 2, name: 'Andrey' },
-        { id: 3, name: 'Sveta' },
-        { id: 4, name: 'Sasha' },
-        { id: 5, name: 'Viktor' },
-        { id: 6, name: 'Valera' } 
-    ],
+    dialogsPage: {
+        dialogs: [
+            { id: 1, name: 'Dimych' },
+            { id: 2, name: 'Andrey' },
+            { id: 3, name: 'Sveta' },
+            { id: 4, name: 'Sasha' },
+            { id: 5, name: 'Viktor' },
+            { id: 6, name: 'Valera' }
+        ],
 
-    messages: [
-        { id: 1, message: 'Hi' },
-        { id: 2, message: 'How is your it-kama?' },
-        { id: 3, message: 'Yo' },
-        { id: 4, message: 'Yo' },
-        { id: 5, message: 'Yo' }
-    ]}
-  
+        messages: [
+            { id: 1, message: 'Hi' },
+            { id: 2, message: 'How is your it-kama?' },
+            { id: 3, message: 'Yo' },
+            { id: 4, message: 'Yo' },
+            { id: 5, message: 'Yo' }
+        ]
+    }
+
 }
+
+export let addPost = (postMessage) => {
+    let newPost = {
+        id: 5,
+        message: postMessage,
+        likesCount: 0,
+    };
+    state.profilePage.posts.push(newPost);
+};
 
 export default state;
