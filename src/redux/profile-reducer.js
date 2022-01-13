e4f810debed45bf7137f2fbac763584522ef37f9
KhroDen <dg.hromov@gmail.com>
2022-01-13 20:24:58 +0200

    lesson 42, подключили redux вместо нашего store

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,16 @@
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 
-const profileReducer = (state, action) => {
+
+let initialState = {
+    posts: [
+        { id: 1, post: 'Hi, how are you?', likesCount: 14 },
+        { id: 2, post: "It's my first post", likesCount: 21 }
+    ],
+    newPostText: 'it-kama'
+};
+
+const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST:
             let newPost = {
