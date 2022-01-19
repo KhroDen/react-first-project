93547107fbf51b79e02da0b5a3c2c5f48faeae1b
KhroDen <dg.hromov@gmail.com>
2022-01-19 14:41:05 +0200

    lesson 49, создали страницу пользователей

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
 import { Route, Routes } from 'react-router';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
+import UsersContainer from './components/Users/UsersContainer';
 
 
 const App = (props) => {
@@ -24,6 +24,7 @@ const App = (props) => {
           <Route path='/news' element={<News />} />
           <Route path='/music' element={<Music />} />
           <Route path='/settings' element={<Settings />} />
+          <Route path='/users' element={<UsersContainer/>} />
         </Routes>
       </div>
     </div>
