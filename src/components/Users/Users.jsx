92356d7443fe2b9530c88f65224b02f04a475a2f
KhroDen <dg.hromov@gmail.com>
2022-01-20 12:55:19 +0200

    lesson 52, side effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,16 +6,17 @@ import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
 
-    if (props.users.length === 0) {
-
-
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(Response => {
-                props.setUsers(Response.data.items);
-            });
-
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(Response => {
+                    props.setUsers(Response.data.items);
+                });
+        }
     }
+
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
