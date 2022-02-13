d7deff032a1eed18c3b6b9565626fc94754cf5fd
KhroDen <dg.hromov@gmail.com>
2022-02-13 15:24:17 +0200

    lesson 94, React.lazy, React.suspense

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
-import React from 'react';
+import React, { Suspense } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import { Route, Routes } from 'react-router';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import LoginPage from "./components/login/Login";
 import { initializeApp } from "./redux/app-reducer";
-import {connect, Provider} from 'react-redux';
+import { connect, Provider } from 'react-redux';
 import Preloader from './components/common/preloader/Preloader';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import store from "./redux/redux-store";
+//import DialogsContainer from './components/Dialogs/DialogsContainer';
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+// import ProfileContainer from './components/Profile/ProfileContainer';
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+
 
 
 const App = (props) => {
@@ -32,16 +35,18 @@ const App = (props) => {
 			<HeaderContainer />
 			<Navbar />
 			<div className='app-wrapper-content'>
-				<Routes>
-					<Route path='/*' element={<ProfileContainer />} />
-					<Route path='/profile/:userId' element={<ProfileContainer />} />
-					<Route path='/dialogs/*' element={<DialogsContainer />} />
-					<Route path='/news' element={<News />} />
-					<Route path='/music' element={<Music />} />
-					<Route path='/settings' element={<Settings />} />
-					<Route path='/users' element={<UsersContainer />} />
-					<Route path='/login' element={<LoginPage />} />
-				</Routes>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Routes>
+						<Route path='/*' element={<ProfileContainer />} />
+						<Route path='/profile/:userId' element={<ProfileContainer />} />
+						<Route path='/dialogs/*' element={<DialogsContainer />} />
+						<Route path='/news' element={<News />} />
+						<Route path='/music' element={<Music />} />
+						<Route path='/settings' element={<Settings />} />
+						<Route path='/users' element={<UsersContainer />} />
+						<Route path='/login' element={<LoginPage />} />
+					</Routes>
+				</Suspense>
 			</div>
 		</div>
 	);
@@ -53,7 +58,7 @@ const mapStateToProps = (state) => ({
 
 let AppContainer = connect(mapStateToProps, { initializeApp })(App);
 
-const SamuraiJSApp = (props)=> {
+const SamuraiJSApp = (props) => {
 	return <BrowserRouter>
 		<React.StrictMode>
 			<Provider store={store}>
