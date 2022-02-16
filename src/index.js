1f65114fbfef3072f0f9dffaa4b78926a134ab3c
KhroDen <dg.hromov@gmail.com>
2022-02-16 17:41:02 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import './index.css';
-import store from './redux/redux-store';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from "react-redux"
 import SamuraiJSApp from "./App";
 
 
 ReactDOM.render(
-    <SamuraiJSApp/> ,
+  <SamuraiJSApp />,
   document.getElementById('root')
 );
 
